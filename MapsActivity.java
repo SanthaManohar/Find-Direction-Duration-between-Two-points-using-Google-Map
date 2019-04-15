9661af6c4daf4ecda5495c06ed40d0da61c520e9
SanthaManohar <49111237+SanthaManohar@users.noreply.github.com>
2019-04-15 07:02:03 +0530

    Direction Between two point

---
+++ MapsActivity.java
@@ -0,0 +1,216 @@
+package basicandroid.com.directiononegit;
+
+import android.Manifest;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+    private static final String TAG = MapsActivity.class.getSimpleName();
+    private GoogleMap mMap;
+    private List<LatLng> latLngList;
+    private TextView distanceValue;
+    private TextView durationValue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        distanceValue = (TextView) findViewById(R.id.distance_value);
+        durationValue = (TextView) findViewById(R.id.duration_value);
+        latLngList = new ArrayList<LatLng>();
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+        mMap.getUiSettings().setMyLocationButtonEnabled(true);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+            @Override
+            public void onMapLongClick(LatLng latLng) {
+
+                if(latLngList.size() > 1){
+                    refreshMap(mMap);
+                    latLngList.clear();
+                    distanceValue.setText("");
+                    durationValue.setText("");
+                }
+                latLngList.add(latLng);
+                Log.d(TAG, "Marker number " + latLngList.size());
+                createMarker(latLng, latLngList.size());
+                if(latLngList.size() == 2){
+                    LatLng origin = latLngList.get(0);
+                    LatLng destination = latLngList.get(1);
+                    //use Google Direction API to get the route between these Locations
+                    String directionApiPath = Helper.getUrl(String.valueOf(origin.latitude), String.valueOf(origin.longitude),
+                            String.valueOf(destination.latitude), String.valueOf(destination.longitude));
+                    Log.d(TAG, "Path " + directionApiPath);
+                    getDirectionFromDirectionApiServer(directionApiPath);
+                }
+
+            }
+        });
+    }
+
+    private void refreshMap(GoogleMap mapInstance){
+        mapInstance.clear();
+    }
+    private void createMarker(LatLng latLng, int position){
+        MarkerOptions mOptions = new MarkerOptions().position(latLng);
+        if(position == 1){
+            mOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW));
+        }else{
+            mOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
+        }
+        addCameraToMap(latLng);
+        mMap.addMarker(mOptions);
+    }
+    private void addCameraToMap(LatLng latLng){
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(latLng)
+                .zoom(8)
+                .build();
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+    }
+    private void getDirectionFromDirectionApiServer(String url){
+        GsonRequest<DirectionObject> serverRequest = new GsonRequest<DirectionObject>(
+                Request.Method.GET,
+                url,
+                DirectionObject.class,
+                createRequestSuccessListener(),
+                createRequestErrorListener());
+        serverRequest.setRetryPolicy(new DefaultRetryPolicy(
+                Helper.MY_SOCKET_TIMEOUT_MS,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        VolleySingleton.getInstance(getApplicationContext()).addToRequestQueue(serverRequest);
+    }
+    private Response.Listener<DirectionObject> createRequestSuccessListener() {
+        return new Response.Listener<DirectionObject>() {
+            @Override
+            public void onResponse(DirectionObject response) {
+                try {
+                    Log.d("JSON Response", response.toString());
+                    if(response.getStatus().equals("OK")){
+                        List<LatLng> mDirections = getDirectionPolylines(response.getRoutes());
+                        drawRouteOnMap(mMap, mDirections);
+                    }else{
+                        Toast.makeText(MapsActivity.this, R.string.server_error, Toast.LENGTH_SHORT).show();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            };
+        };
+    }
+    private void setRouteDistanceAndDuration(String distance, String duration){
+        distanceValue.setText(distance);
+        durationValue.setText(duration);
+    }
+    private List<LatLng> getDirectionPolylines(List<RouteObject> routes){
+        List<LatLng> directionList = new ArrayList<LatLng>();
+        for(RouteObject route : routes){
+            List<LegsObject> legs = route.getLegs();
+            for(LegsObject leg : legs){
+                String routeDistance = leg.getDistance().getText();
+                String routeDuration = leg.getDuration().getText();
+                setRouteDistanceAndDuration(routeDistance, routeDuration);
+                List<StepsObject> steps = leg.getSteps();
+                for(StepsObject step : steps){
+                    PolylineObject polyline = step.getPolyline();
+                    String points = polyline.getPoints();
+                    List<LatLng> singlePolyline = decodePoly(points);
+                    for (LatLng direction : singlePolyline){
+                        directionList.add(direction);
+                    }
+                }
+            }
+        }
+        return directionList;
+    }
+    private Response.ErrorListener createRequestErrorListener() {
+        return new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        };
+    }
+    private void drawRouteOnMap(GoogleMap map, List<LatLng> positions){
+        PolylineOptions options = new PolylineOptions().width(5).color(Color.RED).geodesic(true);
+        options.addAll(positions);
+        Polyline polyline = map.addPolyline(options);
+    }
+    /**
+     * Method to decode polyline points
+     * Courtesy : http://jeffreysambells.com/2010/05/27/decoding-polylines-from-google-maps-direction-api-with-java
+     * */
+    private List<LatLng> decodePoly(String encoded) {
+        List<LatLng> poly = new ArrayList<>();
+        int index = 0, len = encoded.length();
+        int lat = 0, lng = 0;
+        while (index < len) {
+            int b, shift = 0, result = 0;
+            do {
+                b = encoded.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+            int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lat += dlat;
+            shift = 0;
+            result = 0;
+            do {
+                b = encoded.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+            int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lng += dlng;
+            LatLng p = new LatLng((((double) lat / 1E5)),
+                    (((double) lng / 1E5)));
+            poly.add(p);
+        }
+        return poly;
+    }
+}
---
+++ StepsObject.java
@@ -0,0 +1,11 @@
+package basicandroid.com.directiononegit;
+
+public class StepsObject {
+    private PolylineObject polyline;
+    public StepsObject(PolylineObject polyline) {
+        this.polyline = polyline;
+    }
+    public PolylineObject getPolyline() {
+        return polyline;
+    }
+}
